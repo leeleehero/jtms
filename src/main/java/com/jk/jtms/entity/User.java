a95eadf41b07d4c0e2f1d19f6d414232c0542873
wyx <jsntwyx@sina.com>
2019-01-07 13:40:20 +0800

    wyx

---
+++ src/main/java/com/jk/jtms/entity/User.java
@@ -0,0 +1,70 @@
+package com.jk.jtms.entity;
+
+public class User {
+    private String username;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private String sfCode;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSfCode() {
+        return sfCode;
+    }
+
+    public void setSfCode(String sfCode) {
+        this.sfCode = sfCode;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", sfCode='" + sfCode + '\'' +
+                '}';
+    }
+}
